ec3907c4e658a9846220b8e08551d30181e6aede
lersh <lersh99@gmail.com>
2017-10-09 16:00:18 +0800

    捕获找不到域名等错误

--- potatoServer.js
+++ potatoServer.js
@@ -2,9 +2,10 @@
 const net = require('net');
 const Potato = require('./potato');
 const crypto = require('crypto');
+const domain = require('domain');
 
 var server = net.createServer((socket) => {
-    console.log('client connect from  %s:%d', socket.remoteAddress, socket.remotePort);
+    //console.log('client connect from  %s:%d', socket.remoteAddress, socket.remotePort);
 
     var aes = crypto.createCipher('aes-256-cfb', '123qweASD');
     var deAes = crypto.createDecipher('aes-256-cfb', '123qweASD');
@@ -15,21 +16,34 @@ var server = net.createServer((socket) => {
 
     decode.on('head', (remote) => {
         console.log('client want to connect %s:%d', remote.addr, remote.port);
-        net.connect(remote.port, remote.addr, function (err) {//创建一个连接到目标服务器的链接
-            console.log('connect to  %s:%d', remote.addr, remote.port);
-            client
-                .pipe(this)//将过滤头部后的数据发给目标服务器
-                .pipe(aes)//将目标服务器返回的数据加密
-                .pipe(socket);//将加密后的数据返回给potato的客户端
 
-            //处理错误    
-            this.on('error', (err) => {
-                console.log('连接远程服务器出错:' + err.stack);
+        var d = domain.create();//创建一个域，捕获错误用
+        d.on('error', (err) => {
+            switch (err.code) {
+                case 'ENOTFOUND':
+                    console.log('找不到域名: %s', remote.addr);
+                    break;
+                case 'ECONNREFUSED':
+                    console.log('连接被拒绝: %s:%d', remote.addr, remote.port);
+                    break;
+                case 'ECONNRESET':
+                    console.log('连接被中断: %s:%d', remote.addr, remote.port);
+                    break;
+                default:
+                    console.log('域里未处理的错误:' + err.stack);
+            }
+        });
+
+        d.run(() => {//在域里运行代码，错误会被域捕捉
+            net.connect(remote.port, remote.addr, function (err) {//创建一个连接到目标服务器的链接
+                console.log('connect to  %s:%d', remote.addr, remote.port);
+                client
+                    .pipe(this)//将过滤头部后的数据发给目标服务器
+                    .pipe(aes)//将目标服务器返回的数据加密
+                    .pipe(socket);//将加密后的数据返回给potato的客户端
             });
         });
     });
-
-
 });
 
 
@@ -38,6 +52,6 @@ server.listen(1999, () => {
 });
 
 process.on('uncaughtException', function (err) {
-	console.log("捕获未处理的错误: " + err.message);
-	console.log(err.stack);
+    console.log("捕获未处理的错误: " + err.message);
+    console.log(err.stack);
 });
