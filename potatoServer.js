a1fb829ca1c40759a6486092850312dcd4bb56ee
lersh <lersh99@gmail.com>
2017-10-09 10:07:06 +0800

    修改

--- potatoServer.js
+++ potatoServer.js
@@ -1,18 +1,18 @@
 'use strict'
 const net = require('net');
-const PotatoDecode = require('./potatoDecode');
+const Potato = require('./potato');
 const crypto = require('crypto');
 
 var server = net.createServer((socket) => {
     console.log('client connect from  %s:%d', socket.remoteAddress, socket.remotePort);
 
     var deAes = crypto.createDecipher('aes-256-cfb', '123qweASD');
-    var potatoDecode = new PotatoDecode();
+    var decode = new Potato();
     var client = socket
         .pipe(deAes)//解密
-        .pipe(potatoDecode);//过滤头部
+        .pipe(decode);//过滤头部
 
-    potatoDecode.on('head', (remote) => {
+    decode.on('head', (remote) => {
         console.log('client want to connect %s:%d', remote.addr, remote.port);
         net.connect(remote.port, remote.addr, function (err) {
             console.log('connect to  %s:%d', remote.addr, remote.port);
