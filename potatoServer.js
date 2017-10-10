c6ae602602ca02af8433b4b6ff87024030eb4cdc
lersh <lersh99@gmail.com>
2017-10-10 16:59:19 +0800

    增加log日志

--- potatoServer.js
+++ potatoServer.js
@@ -2,6 +2,11 @@
 const net = require('net');
 const crypto = require('crypto');
 const domain = require('domain');
+//log4js module
+var log4js = require('log4js');
+var logConfig = require('./logConfig.json');
+log4js.configure(logConfig);
+var logger = log4js.getLogger('server');
 
 var Potato = require('./potato');
 
@@ -16,8 +21,7 @@ var potatoServer = net.createServer((pototaClient) => {
 
     pototaClient.once('data', (data) => {
         var reqHead = Potato.ResolveHead.ConnectRequest(data);//解析请求头
-        console.log('want to connect %s:%d\r\n', reqHead.dst.addr, reqHead.dst.port);
-        //console.dir(reqHead);
+        logger.trace('want to connect %s:%d\r\n', reqHead.dst.addr, reqHead.dst.port);
 
         var sig;//返回信号
         var d = domain.create();//用来捕捉错误信号的域
@@ -27,7 +31,7 @@ var potatoServer = net.createServer((pototaClient) => {
             var proxySocket = net.connect(reqHead.dst.port, reqHead.dst.addr);
             //如果连上了
             proxySocket.on('connect', function () {
-                //console.log('connected %s:%d\r\n', this.remoteAddress, this.remotePort);
+                logger.trace('connected %s:%d\r\n', this.remoteAddress, this.remotePort);
                 sig = Potato.CreateHead.ConnectReply(Potato.ReplyCode.SUCCEEDED);//创建一个成功信号
                 pototaClient.write(sig);//如果连上了就发送成功信号                
                 var cipher = crypto.createCipher(algorithm, password),
@@ -44,28 +48,28 @@ var potatoServer = net.createServer((pototaClient) => {
         d.on('error', (err) => {
             switch (err.code) {
                 case 'ENOTFOUND':
-                    console.log('找不到域名: %s', reqHead.addr);
+                    logger.info('找不到域名: %s', reqHead.addr);
                     sig = Potato.CreateHead.ConnectReply(Potato.ReplyCode.HOST_UNREACHABLE);
                     pototaClient.write(sig);
                     break;
                 case 'ECONNREFUSED':
-                    console.log('连接被拒绝: %s:%d', reqHead.addr, reqHead.port);
+                    logger.info('连接被拒绝: %s:%d', reqHead.addr, reqHead.port);
                     sig = Potato.CreateHead.ConnectReply(Potato.ReplyCode.CONNECTION_REFUSED);
                     pototaClient.write(sig);
                     break;
                 case 'ECONNRESET':
-                    console.log('连接被中断: %s:%d', reqHead.addr, reqHead.port);
+                    logger.info('连接被中断: %s:%d', reqHead.addr, reqHead.port);
                     sig = Potato.CreateHead.ConnectReply(Potato.ReplyCode.CONNECTION_NOT_ALLOWED);
                     if (pototaClient.writable)
                         pototaClient.write(sig);
                     break;
                 case 'ETIMEDOUT':
-                    console.log('连接超时: %s:%d', reqHead.addr, reqHead.port);
+                    logger.info('连接超时: %s:%d', reqHead.addr, reqHead.port);
                     sig = Potato.CreateHead.ConnectReply(Potato.ReplyCode.NETWORK_UNREACHABLE);
                     pototaClient.write(sig);
                     break;
                 default:
-                    console.log('域里未处理的错误:' + err.stack);
+                    logger.info('域里未处理的错误:' + err.stack);
                     sig = Potato.CreateHead.ConnectReply(Potato.ReplyCode.GENERAL_FAILURE);
                     pototaClient.write(sig);
             }
@@ -77,10 +81,10 @@ var potatoServer = net.createServer((pototaClient) => {
 
 
 potatoServer.listen(1999, () => {
-    console.log('listening on 1999');
+    logger.info('listening on 1999');
 });
 
 process.on('uncaughtException', function (err) {
-    console.log("捕获未处理的错误: " + err.message);
-    console.error(err);
+    logger.error("捕获未处理的错误: " + err.message);
+    logger.error(err);
 });
