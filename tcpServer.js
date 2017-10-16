9fff3bcf7a67255dc77a28387e90a4c11b42a38f
lersh <lersh99@gmail.com>
2017-10-16 15:20:05 +0800

    test

--- tcpServer.js
+++ tcpServer.js
@@ -2,16 +2,17 @@
 const net = require('net');
 const stream = require('stream');
 
+var i = 1;
 const transform = stream.Transform({
     highWaterMark: 2,
     transform: function (buf, enc, next) {
-        console.log('get', buf.length);
+        console.log(i++, 'get', buf.length);
         next(null, buf)
     }
 })
 
 var server = net.createServer((clientSocket) => {
-    var rs = require('fs').createReadStream('./Test_Ciphered.MOV')
+    var rs = require('fs').createReadStream('./test_Ciphered.png')
     rs.pipe(transform).pipe(clientSocket);
 });
 
