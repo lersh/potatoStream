d6ae831faeb3369a05f753372d7f9c662fedd32e
lersh <lersh99@gmail.com>
2017-10-31 11:22:21 +0800

    bugfix

--- cert.js
+++ cert.js
@@ -1,17 +1,17 @@
-var exec = require('child_process').exec;
-var config = require('./config.json');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const config = require('./config.json');
 
 var password = config.password;
+var fakeHost = config.camouflage;
 var certDir = './cert_test/';
-var keyStr = `openssl genrsa -aes256 -passout pass:${password} -out ${certDir}ca.key 2048`;
+var caKeyStr = `openssl genrsa -aes256 -passout pass:${password} -out ${certDir}ca.key 2048`;
+var caCsrStr = `openssl req -new -passin pass:${password} -key ${certDir}ca.key -out ${certDir}ca.csr -subj "/C=US/O=VeriSign, Inc./OU=VeriSign Trust Network/OU=(c) 2006 VeriSign, Inc. - For authorized use only/CN=VeriSign Class 3 Public Primary Certification Authority - G5"`;
 
 console.log(keyStr);
 
-exec(keyStr, (error, stdout, stderr) => {
-    if (error) {
-        console.log(stderr);
-
-    } else {
-
-    }
-});
+(async () => {
+    const { stdout, stderr } = await exec(caCsrStr);
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+})();
