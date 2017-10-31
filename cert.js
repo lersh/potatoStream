2372a9320aee60978e18cb284749cd119941c500
lersh <lersh99@gmail.com>
2017-10-31 11:26:09 +0800

    bug fix

--- cert.js
+++ cert.js
@@ -7,11 +7,13 @@ var fakeHost = config.camouflage;
 var certDir = './cert_test/';
 var caKeyStr = `openssl genrsa -aes256 -passout pass:${password} -out ${certDir}ca.key 2048`;
 var caCsrStr = `openssl req -new -passin pass:${password} -key ${certDir}ca.key -out ${certDir}ca.csr -subj "/C=US/O=VeriSign, Inc./OU=VeriSign Trust Network/OU=(c) 2006 VeriSign, Inc. - For authorized use only/CN=VeriSign Class 3 Public Primary Certification Authority - G5"`;
+var caCertStr = `openssl x509 -req -days 3650 -in ${certDir}ca.csr -signkey ${certDir}ca.key -passin pass:${password} -out ${certDir}ca.crt`;
 
-console.log(caCsrStr);
+console.log(caCertStr);
 
 (async () => {
-    const { stdout, stderr } = await exec(caCsrStr);
+    const { error, stdout, stderr } = await exec(caCertStr);
+    console.log('error:', error);
     console.log('stdout:', stdout);
     console.log('stderr:', stderr);
 })();
