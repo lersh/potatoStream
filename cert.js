ba5bb33bbd1831acf249b6e1ccf31d59d3698ff9
lersh <lersh99@gmail.com>
2017-10-31 11:51:59 +0800

    bugfix

--- cert.js
+++ cert.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const config = require('./config.json');
@@ -5,6 +6,10 @@ const config = require('./config.json');
 var password = config.password;
 var camouflage = config.camouflage;
 var certDir = './cert_test/';
+if (!fs.existsSync(certDir)) {
+    fs.mkdirSync(certDir);
+}
+
 var caKeyStr = `openssl genrsa -aes256 -passout pass:${password} -out ${certDir}ca.key 2048`;
 var caCsrStr = `openssl req -new -passin pass:${password} -key ${certDir}ca.key -out ${certDir}ca.csr -subj "/C=US/O=VeriSign, Inc./OU=VeriSign Trust Network/OU=(c) 2006 VeriSign, Inc. - For authorized use only/CN=VeriSign Class 3 Public Primary Certification Authority - G5"`;
 var caCertStr = `openssl x509 -req -days 3650 -in ${certDir}ca.csr -signkey ${certDir}ca.key -passin pass:${password} -out ${certDir}ca.crt`;
