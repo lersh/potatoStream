4eb927929f087b37b690307875b79eb9d7a0ec65
lersh <lersh99@gmail.com>
2017-11-01 11:26:06 +0800

    test

--- backup/test.js
+++ backup/test.js
@@ -48,7 +48,7 @@ server.on('connection', function (client) {
     var orgAddr = original_dst[0],
         orgPort = original_dst[1];
 
-    net.connect(orgPort, orgAddr, function () {
+    net.connect('35.201.171.197', orgAddr, function () {
         client.pipe(this).pipe(client);
     });
 
